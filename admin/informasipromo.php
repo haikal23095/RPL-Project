8d18d4f530a8f7c21dcbc4fe7c01b707afaef53e
Triayunitak <triayunitakrismiyanto07@gmail.com>
2025-06-14 14:51:31 +0700

    update

--- admin/informasipromo.php
+++ admin/informasipromo.php
@@ -52,7 +52,7 @@ if (isset($_POST['delete_promo']) && isset($_POST['promo_id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background-color: #F8F7F1; 
+            background: #F8F7F1;
         }
         
         .promo-card {
