df341f18554d18efea19e655be9573a069ef1893
Triayunitak <triayunitakrismiyanto07@gmail.com>
2025-06-14 23:22:52 +0700

    update

--- user/chat.php
+++ user/chat.php
@@ -51,6 +51,13 @@ $result = mysqli_query($kon, $query);
     <?php include 'aset.php'; ?>
 
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Andika:ital,wght@0,400;0,700;1,400;1,700&family=Pixelify+Sans:wght@400..700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Aclonica&family=Andika:ital,wght@0,400;0,700;1,400;1,700&family=Pixelify+Sans:wght@400..700&display=swap');
+        body {
+            background: #F8F7F1 !important;
+            font-family: 'Andika', sans-serif;
+            color: #2D3A3A !important;
+        }
         .chat-card {
             display: flex;
             flex-direction: column;
