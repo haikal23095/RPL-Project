d8fa664ff69cc2578754e83282908140f17e2d4a
haikal23095 <230411100095@student.trunojoyo.ac.id>
2025-06-15 14:49:42 +0700

    done reset password

--- login.php
+++ login.php
@@ -12,10 +12,9 @@ use Twilio\Rest\Client;
 // $dotenv->load();
 
 
-$sid = '';
-$token = '';
-$verifySid = '';
-
+$sid = 'AC4fcb38388f5c58c449b150823cf9b4eb';
+$token = '43275be86fd2e9cd95758de41186f0de';
+$verifySid = 'VA1c3e751033905756f2848f4aeb7f4b0c';
 
 
 // Debug sementara
